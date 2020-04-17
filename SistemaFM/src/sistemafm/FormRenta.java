f810392bd8504c8a873120dd472de65585c52e5e
Ankor-sys <sebasmoreira4@gmail.com>
2020-04-16 21:46:26 -0500

    Conversion de Formulario Renta a Internal Frame

--- SistemaFM/src/sistemafm/FormRenta.java
+++ SistemaFM/src/sistemafm/FormRenta.java
@@ -15,23 +15,17 @@ import javax.swing.table.DefaultTableModel;
  *
  * @author SEBAS
  */
-public class FormularioRenta extends javax.swing.JFrame {
-
-     private static String db = "SISTEMA_FM";
+public class FormRenta extends javax.swing.JInternalFrame {
+    private static String db = "sistema_fm";
     private static String user = "root";
     private static String password = "Cagada1234";
     private static String host = "localhost";
     private static String server = "jdbc:mysql://"+ host + "/" +db; 
-    
- 
     /**
-     * Creates new form FormularioRenta
+     * Creates new form FormRenta
      */
-    public FormularioRenta() {
+    public FormRenta() {
         initComponents();
-        
-        
-        
     }
 
     /**
@@ -43,29 +37,14 @@ public class FormularioRenta extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tblArticulos = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         txtNumeroTarjeta = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         txtNombreArticulo = new javax.swing.JTextField();
         btnBuscar = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        tblArticulos = new javax.swing.JTable();
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jLabel1.setText("FORMULARIO DE RENTA");
-
-        jLabel2.setText("No. Tarjeta: ");
-
-        jLabel3.setText("Nombre de Articulo : ");
-
-        btnBuscar.setText("BUSCAR");
-        btnBuscar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnBuscarActionPerformed(evt);
-            }
-        });
 
         tblArticulos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -79,6 +58,19 @@ public class FormularioRenta extends javax.swing.JFrame {
             }
         ));
         jScrollPane1.setViewportView(tblArticulos);
+
+        jLabel1.setText("FORMULARIO DE RENTA");
+
+        jLabel2.setText("No. Tarjeta: ");
+
+        jLabel3.setText("Nombre de Articulo : ");
+
+        btnBuscar.setText("BUSCAR");
+        btnBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBuscarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -104,7 +96,7 @@ public class FormularioRenta extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(46, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -133,58 +125,24 @@ public class FormularioRenta extends javax.swing.JFrame {
         // TODO add your handling code here:
         DefaultTableModel modelo  =  new DefaultTableModel();
         modelo.setColumnIdentifiers(new Object[]{"Id_Articulo", "Tipo", "Nombre", "Genero" , "Descripcion", "Precio_Renta"});
-        
+
         try {
             Connection cn = DriverManager.getConnection(server, user, password);
             PreparedStatement pst = cn.prepareStatement("select * from articulos where Nombre = ?");
             pst.setString(1, txtNombreArticulo.getText().trim());
-            
+
             ResultSet rs = pst.executeQuery();
-        
-             while(rs.next()){
-            modelo.addRow(new Object[]{rs.getString("Id_Articulo"), rs.getString("Tipo"),
-                   rs.getString("Nombre"), rs.getString("Genero"), rs.getString("Descripcion"),rs.getString("Precio_Renta")});
-            }
-            
-             tblArticulos.setModel(modelo);
+
+            while(rs.next()){
+                modelo.addRow(new Object[]{rs.getString("Id_Articulo"), rs.getString("Tipo"),
+                    rs.getString("Nombre"), rs.getString("Genero"), rs.getString("Descripcion"),rs.getString("Precio_Renta")});
+        }
+
+        tblArticulos.setModel(modelo);
         }
         catch(Exception e){}
     }//GEN-LAST:event_btnBuscarActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FormularioRenta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FormularioRenta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FormularioRenta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FormularioRenta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new FormularioRenta().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnBuscar;
